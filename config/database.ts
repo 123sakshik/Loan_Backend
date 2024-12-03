9b879fb2e146ec6a4aee16bd82cd0a0076065ce4
Sakshi <sakkolhe2552@gmail.com>
2024-12-03 21:02:28 +0530

    Initial commit

---
+++ config/database.ts
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+const config = require('../config/config.json');   
+// allows developers to interact with databases using JavaScript objects and methods, 
+// making it easier to work with databases without writing raw SQL queries. 
+// Sequelize supports various database management systems, such as PostgreSQL, MySQL, SQLite, and MSSQL.
+
+const sequel = new Sequelize(config.development);
+
+module.exports = sequel;
